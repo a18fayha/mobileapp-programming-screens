54b17533462e7aaaf02bc35fce7a16a471cb54cf
a18fayha <62876354+a18fayha@users.noreply.github.com>
2022-04-25 10:05:33 +0200

    Added a string to the intent bundle that shows in the second activity

--- app/src/main/java/com/example/screens/MainActivity.java
+++ app/src/main/java/com/example/screens/MainActivity.java
@@ -7,10 +7,11 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    Button button;
+   private  Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,10 +19,13 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         button = findViewById(R.id.button);
+
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
                 Intent intent =  new Intent(MainActivity.this, SecondActivity.class);
+                intent.putExtra("name", "Faysal");
                 startActivity(intent);
             }
         });
--- app/src/main/java/com/example/screens/SecondActivity.java
+++ app/src/main/java/com/example/screens/SecondActivity.java
@@ -3,12 +3,23 @@ package com.example.screens;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+
+    private TextView myName;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        myName = findViewById(R.id.data);
+
+        Bundle extras = getIntent().getExtras();
+        if(extras != null){
+            String name = extras.getString("name");
+            myName.setText(name);
+        }
+
     }
 }
